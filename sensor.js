8d4e549f5e46efb99f9db738c2e6ef0d149c609b
mozart953 <wilsonnianopop@gmail.com>
2023-05-08 17:28:03 -0600

    adding traffic

--- sensor.js
+++ sensor.js
@@ -9,18 +9,21 @@ class Sensor {
     this.readings=[];
   }
 
-  update(roadBorders) {
+  update(roadBorders,traffic) {
     this.#catRays();
     this.readings=[];
     for(let i=0;i<this.rays.length;i++){
       this.readings.push(
-        this.#getReading(this.rays[i], roadBorders)
+        this.#getReading(
+          this.rays[i],
+           roadBorders,
+           traffic)
       );
     }
 
   }
 
-  #getReading(ray, roadBorders){
+  #getReading(ray, roadBorders,traffic){
     let touches=[];
     
     for(let i=0;i<roadBorders.length;i++){
@@ -33,10 +36,25 @@ class Sensor {
 
       if(touch){
         touches.push(push);
-      }else{
-        const offsets=touches.map(e=>e.offsets);
-        const minOffset=Math.min(...offsets);
-        return touches.find(e=>e.offsets==minOffset);
+      }
+      // else{
+      //   const offsets=touches.map(e=>e.offsets);
+      //   const minOffset=Math.min(...offsets);
+      //   return touches.find(e=>e.offsets==minOffset);
+      // }
+    }
+    for(let i=0;i<traffic.length;i++){
+      const poly=traffic[i].polygon;
+      for(let j=0;j<poly.length;j++){
+        const value=getIntersection(
+          ray[0],
+          ray[1],
+          poly[j],
+          poly[(j+1)%poly.length]
+        );
+        if(value){
+          touches.push(value);
+        }
       }
     }
 
